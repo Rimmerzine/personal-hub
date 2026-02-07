a085c8e07d123811fa02ef4df032a007f11e261e
Rimmerzine <44525377+Rimmerzine@users.noreply.github.com>
2026-02-07 23:22:16 +0000

    Reworked the HomeControllerSpec to be much more of a unit test - Introduced Mockito to mock the view instead of injecting the real view - Utilise beforeAndAfterEach to reset the mock before each test - Produced a BaseFrontendController to implicitly take a MessagesControllerComponents - Produced a BaseControllerSpec to hold things which may be common for future controller tests - Removed redundant tests from the HomeControllerSpec

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,51 +1,38 @@
 package controllers
 
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import play.api.test._
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.mvc.Result
 import play.api.test.Helpers._
+import scalatags.Text.all.html
 import views.Home
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+import scala.concurrent.Future
 
-  "HomeController GET" should {
+class HomeControllerSpec extends BaseControllerSpec with MockitoSugar with BeforeAndAfterEach {
 
-    "render the Index page from a new instance of controller" in {
-      val homeView: Home = inject[Home]
-      val controller = new HomeController(homeView, stubMessagesControllerComponents())
-      val home = controller.show().apply(FakeRequest(GET, "/"))
+  val mockView: Home = mock[Home]
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("home.heading") // TODO: not great, stubMessagesControllerComponents provides fake messages
-    }
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockView)
+  }
 
-    "render the Index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.show().apply(FakeRequest(GET, "/"))
+  object TestHomeController extends HomeController(
+    mockView
+  )
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Personal Hub")
-      contentAsString(home) must include ("In this personal hub, I will be playing around with Play!")
-      contentAsString(home) must include ("I intend to develop this in small steps.")
-    }
+  "HomeController.show" must {
+    "return OK with HTML content" in {
+      when(mockView()(ArgumentMatchers.any())).thenReturn(html())
 
-    "render the Index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
+      val result: Future[Result] = TestHomeController.show()(request)
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Personal Hub")
-      contentAsString(home) must include ("In this personal hub, I will be playing around with Play!")
-      contentAsString(home) must include ("I intend to develop this in small steps.")
+      status(result) mustBe OK
+      contentType(result) mustBe Some(HTML)
     }
   }
+
 }
